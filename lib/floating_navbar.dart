e6837e31ecadc297fbbcebb82f5ec5a373eafbed
iamngoni <el.nick.ngonie@gmail.com>
2020-10-28 15:43:31 +0200

    Initial commit and possibly final 😂

---
+++ lib/floating_navbar.dart
@@ -0,0 +1,99 @@
+library floating_navbar;
+
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class FloatingNavBar extends StatefulWidget {
+  int index;
+  List pages;
+  List<Icon> icons;
+  Color color;
+
+  FloatingNavBar({
+    Key key,
+    this.index = 0,
+    @required this.pages,
+    @required this.color,
+  });
+
+  @override
+  _FloatingNavBarState createState() => _FloatingNavBarState();
+}
+
+class _FloatingNavBarState extends State<FloatingNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            widget.pages[widget.index],
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 20,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 15.0,
+                ),
+                child: Container(
+                  height: 70,
+                  width: MediaQuery.of(context).size.width,
+                  child: Card(
+                    elevation: 15.0,
+                    color: Color(0xFF6B00F2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: _widgetsBuilder(widget.icons),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _floatingNavBarItem(Icon icon, int index) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () => _changePage(index),
+          child: icon,
+        ),
+        Container(
+          height: 5,
+          width: 5,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: widget.index == index ? Colors.white : Colors.transparent,
+          ),
+        ),
+      ],
+    );
+  }
+
+  List _widgetsBuilder(List<Icon> icons) {
+    List _floatingNavBarItems = [];
+    for (int i = 0; i < icons.length; i++) {
+      Widget item = this._floatingNavBarItem(icons[i], i);
+      _floatingNavBarItems.add(item);
+    }
+    return _floatingNavBarItems;
+  }
+
+  _changePage(index) {
+    setState(() {
+      widget.index = index;
+    });
+  }
+}

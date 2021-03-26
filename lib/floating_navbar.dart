fcebd3a028cfa6076369f9bafaf2e9c089646e35
xanf-code <darshanaswath@gmail.com>
2021-03-26 11:27:19 +0530

    Icon

--- lib/floating_navbar.dart
+++ lib/floating_navbar.dart
@@ -10,7 +10,7 @@ class FloatingNavBar extends StatefulWidget {
   /// [FloatingNavbar] is a simple navigation bar that floats on top of pages at the bottom
   int index;
   List<Widget> pages;
-  List<IconButton> icons;
+  List<Icon> icons;
   Color color;
   Color iconColor;
   double horizontalPadding;
@@ -83,7 +83,7 @@ class _FloatingNavBarState extends State<FloatingNavBar> {
 
   /// [_floatingNavBarItem] will build and return a [FloatingNavBar] item widget
   Widget _floatingNavBarItem(
-      IconButton icon, int index, Color color, bool hapticFeedback) {
+      Icon icon, int index, Color color, bool hapticFeedback) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -113,7 +113,7 @@ class _FloatingNavBarState extends State<FloatingNavBar> {
 
   /// [_widgetsBuilder] adds widgets from [_floatingNavBarItem] into a List<Widget> and returns the list
   List<Widget> _widgetsBuilder(
-      List<IconButton> icons, Color color, bool hapticFeedback) {
+      List<Icon> icons, Color color, bool hapticFeedback) {
     List<Widget> _floatingNavBarItems = [];
     for (int i = 0; i < icons.length; i++) {
       Widget item =
